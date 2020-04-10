639c2713acb34528d1bfe1ffd06fba50a326ce89
matttrue <mrdeevs@gmail.com>
2020-04-10 04:59:06 -0500

    during map generation, cache the first open space so that we can place the player there each spawn. removed any additional logging

--- Kodable Maze/Assets/Scripts/MapGenerator.cs
+++ Kodable Maze/Assets/Scripts/MapGenerator.cs
@@ -11,6 +11,7 @@ public class MapGenerator : MonoBehaviour
     private List<char> walls = new List<char>() { '-', '+', '|' };
     private List<char> spaces = new List<char>() { ' ' };
     private const string NEWLINE = "\n";
+    private Vector3 startingSpace = Vector3.zero;
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +20,6 @@ public class MapGenerator : MonoBehaviour
         {
             // convert the map data text file into a string
             string mazeStr = mazeFile.text;
-            Debug.Log("Original maze length: " + mazeStr.Length);
 
             // map starts at 0,0
             // also find out the dimensions using new line sub str
@@ -29,37 +29,35 @@ public class MapGenerator : MonoBehaviour
             int x = 0;
             int y = height - 1;
 
-            Debug.Log("map width : " + width + " map height : " + height + " input data length : " + mazeStr.Length);
-
             // now that we already have dimensions, let's strip out new line
             // characters because it's impacting parsing
             mazeStr = mazeStr.Replace("\n\n", NEWLINE);
-            Debug.Log("Formatted maze length: " + mazeStr.Length);
 
             // iterate over each tile and build the map
             for (int i = 0; i < mazeStr.Length; i++)
             {
                 if (walls.Contains(mazeStr[i]))
                 {
-                    Debug.Log("Wall Found.");
                     // wall char found
                     // create a new wall
                     Instantiate(wallPrefab, new Vector3(x, y, 0f), Quaternion.identity);
                 }
                 else if (spaces.Contains(mazeStr[i]))
                 {
-                    Debug.Log("Space Found.");
                     // space found, open
                     // create a new space
-                    Instantiate(spacePrefab, new Vector3(x, y, 0f), Quaternion.identity);
-                }
-                else
-                {
-                    if (mazeStr[i] == '\n') Debug.Log("New Line (n) Found.");
-                    if (mazeStr[i] == '\r') Debug.Log("New Line (r) Found.");
+                    Vector3 spaceLoc = new Vector3(x, y, 0f);
+
+                    Instantiate(spacePrefab, spaceLoc, Quaternion.identity);
+
+                    // store and remember where we should place the player
+                    if(startingSpace == Vector3.zero)
+                    {
+                        startingSpace = spaceLoc;
+                        Debug.Log("starting space: " + startingSpace);
+                    }
                 }
 
-                // increment the x marker 
                 x++;
 
                 if (x == width)
@@ -74,7 +72,7 @@ public class MapGenerator : MonoBehaviour
 
             // INIT PLAYER
             // initialize the player at the top left (0, height)
-            player.transform.position = new Vector3(0f, height - 1, 0f);
+            player.transform.position = startingSpace;
         }
         else
         {
