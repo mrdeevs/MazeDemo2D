a6dcfeea004f67de9ab7629eba4b4d550db3e3be
matttrue <mrdeevs@gmail.com>
2020-04-10 03:22:41 -0500

    added a new camera script - the camera will smoothly interpolate after the player

---
+++ Kodable Maze/Assets/Scripts/MazeCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeCamera : MonoBehaviour
+{
+    public Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // animate the camera towards centering on
+        // the player while its not there yet
+        transform.position = new Vector3(
+            transform.position.x, 
+            Mathf.Lerp(transform.position.y, player.transform.position.y, 1.5f * Time.deltaTime), 
+            transform.position.z);
+    }
+}
